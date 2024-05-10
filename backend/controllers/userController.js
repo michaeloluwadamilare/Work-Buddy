63209fe7d0c017025fca04dbec86fb51cd86e885
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2024-05-10 16:49:57 +0100

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 const login = async (req, res) => {
     const { email, password } =  req.body;
@@ -22,7 +27,8 @@ const signup = async (req, res) => {
     try{
 
         const user = await User.signup(email, password)
-        res.status(200).json({user})
+        const token = createToken(user._id);
+        res.status(200).json({email, token})
 
     }catch(error){
 
